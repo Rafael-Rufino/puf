41b8d1b0a2d26e33c5301838e253450e31083233
Rafael Rufino dos Santos <rafaelrufino.52@gmail.com>
2025-05-10 18:20:27 -0300

    feat: Add user routes and handlers

---
+++ api/src/index.js
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+import { router } from './routes'
+
+const app = new Koa()
+
+app.use(bodyParser())
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running on port ${process.env.SERVER_PORT}`)
+})
---
+++ api/src/modules/users/index.js
@@ -0,0 +1,13 @@
+export const list = ctx => {
+  ctx.body = 'Hello '
+}
+
+export const create = ctx => {
+  ctx.body = 'User created'
+}
+export const update = ctx => {
+  ctx.body = 'User updated'
+}
+export const remove = ctx => {
+  ctx.body = 'User removed'
+}
